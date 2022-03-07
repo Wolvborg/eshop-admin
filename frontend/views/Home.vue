9a83486852c1b7e9ccd296a144595d8fc538cf93
Wolvborg <binnu24668@gmail.com>
2022-03-07 21:32:24 +0530

    Tailwind added

--- frontend/views/Home.vue
+++ frontend/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './frontend/App.vue',
+    './frontend/components/**/*.{vue,js,ts,jsx,tsx}',
+    './frontend/views/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
